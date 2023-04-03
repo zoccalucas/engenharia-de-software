0c4816e653ae49c666fcb5de6b8d14e2a674bcf8
zoccalucas <lucaszoccasoares@gmail.com>
2023-04-03 12:02:22 -0300

    test: adiciona os testes propostos

---
+++ __tests__/index.test.ts
@@ -0,0 +1,102 @@
+function realizarMaisCursos(aluno) {
+    if (aluno.media > 7) {
+        aluno.cursosRealizados += 3;
+    }
+}
+
+function verificaSeOPlanoEstaAtivo(aluno) {
+    if (!aluno.planoAtivo) {
+        return 'Renove sua assinatura antes de realize mais cursos'
+    }
+        return 'Plano ativo'
+}
+
+function verificaSeExistemMaisCursosDisponiveis(aluno) {
+    const quantidadeDeCursosDisponiveis = 10;
+
+    if (aluno.cursosRealizados >= quantidadeDeCursosDisponiveis) {
+        return {
+            informacao: 'Aluno atingiu o limite de cursos disponíveis',
+            valorBooleano: false,
+        }
+    }
+        return {
+            informacao: 'Aluno com cursos disponíveis',
+            valorBooleano: true,
+        }
+}
+
+describe('realizarMaisCursos', () => {
+    it('não permite realizar mais 3 cursos se a média do aluno for menor ou igual a 7.0', () => {
+        const aluno = {
+            nome: 'Maria',
+            media: 8.0,
+            cursosRealizados: 4,
+            cursosAdicionais: false,
+            planoAtivo: true,
+        };
+
+        realizarMaisCursos(aluno);
+
+        expect(aluno.media).toBeLessThanOrEqual(7);
+        expect(aluno.cursosRealizados).toBe(4);
+    });
+
+    it('permite realizar mais 3 cursos se a média do aluno for maior que 7.0', () => {
+        const aluno = {
+            nome: 'João',
+            media: 8.0,
+            cursosRealizados: 2,
+            cursosAdicionais: false,
+            planoAtivo: true,
+        };
+
+        realizarMaisCursos(aluno);
+
+        expect(aluno.media).toBeGreaterThanOrEqual(7);
+        expect(aluno.cursosRealizados).toBe(5);
+    });
+    it('permite realizar mais 3 cursos se a média do aluno for maior que 7.0 e ele ja realizou 3 cursos adicionais', () => {
+        const aluno = {
+            nome: 'João',
+            media: 8.0,
+            cursosRealizados: 5,
+            cursosAdicionais: true,
+            planoAtivo: true,
+        };
+
+        realizarMaisCursos(aluno);
+        
+        expect(aluno.cursosAdicionais).toBe(true);
+    });
+    it('não permite realizar mais cursos se o plano expirou e solicita a renovação da assinatura', () => {
+        const aluno = {
+            nome: 'João',
+            media: 8.0,
+            cursosRealizados: 5,
+            cursosAdicionais: true,
+            planoAtivo: false,
+        };
+
+        realizarMaisCursos(aluno);
+        verificaSeOPlanoEstaAtivo(aluno);
+
+        expect(aluno.planoAtivo).toBe(false);
+        expect(verificaSeOPlanoEstaAtivo(aluno)).toBe('Renove sua assinatura antes de realize mais cursos');
+    });
+    it('não permite realizar mais cursos se não tiver mais cursos disponíveis', () => {
+        const aluno = {
+            nome: 'João',
+            media: 8.0,
+            cursosRealizados: 10,
+            cursosAdicionais: true,
+            planoAtivo: false,
+        };
+
+        const {informacao, valorBooleano } = verificaSeExistemMaisCursosDisponiveis(aluno);
+
+        expect(valorBooleano).toBe(false);
+        expect(informacao).toBe('Aluno atingiu o limite de cursos disponíveis');
+        
+    });
+});
