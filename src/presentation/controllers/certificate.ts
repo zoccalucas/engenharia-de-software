1001a289e36b493ffd07871d09ee07acf062cef7
zoccalucas <lucaszoccasoares@gmail.com>
2023-05-22 16:32:05 -0300

    refactor: simplify certificates controller

--- src/presentation/controllers/certificate.ts
+++ src/presentation/controllers/certificate.ts
@@ -1,19 +1,14 @@
 import { HttpRequest, HttpResponse } from '../protocols/http';
 import { MissingParamError } from '../errors/missing-param-error';
+import { badRequest } from '../helpers/http-helper';
 
 export class CertificateController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.studentId) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('studentId'),
-      };
-    }
-    if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('studentEmail'),
-      };
+    const requiredFields = ['studentId', 'studentEmail'];
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
     }
     return {
       statusCode: 200,
