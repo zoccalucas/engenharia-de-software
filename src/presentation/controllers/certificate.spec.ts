d83e261c4cc0f7efdf1b1eed53f7b954ee659aa5
zoccalucas <lucaszoccasoares@gmail.com>
2023-05-22 15:10:08 -0300

    test: ensure CertificateController return an error if no email is provided

--- src/presentation/controllers/certificate.spec.ts
+++ src/presentation/controllers/certificate.spec.ts
@@ -1,21 +1,33 @@
-import { CertificateController } from './certificate'
+import { CertificateController } from './certificate';
 
 describe('Certificate Controller', () => {
   test('Should return 400 if no studentId is provided', async () => {
     const sut = new CertificateController();
-    
+
     const httpRequest = {
       body: {
-        email: 'any_email@gmail.com',
-        cursos: ['Curso 1', 'Curso 2', 'Curso 3'],
-        cursosFinalizados: ['Curso 1', 'Curso 2'],
-        quantidadeDeCursosDisponives: 1,
+        studentEmail: 'any_email@gmail.com',
         planoAtivo: true,
-      }
-    }
+      },
+    };
 
-      const httpResponse = sut.handle(httpRequest)
-      expect(httpResponse.statusCode).toBe(400)
-      expect(httpResponse.body).toEqual(new Error('Missing param: studentId'))
-})
-})
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: studentId'));
+  });
+
+  test('Should return 400 if no email is provided', async () => {
+    const sut = new CertificateController();
+
+    const httpRequest = {
+      body: {
+        studentId: Number,
+        planoAtivo: true,
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: studentId'));
+  });
+});
