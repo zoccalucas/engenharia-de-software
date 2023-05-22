f34e9c1dca950a964596ab61f6d1f49dc90381f0
zoccalucas <lucaszoccasoares@gmail.com>
2023-05-22 17:35:45 -0300

    refactor: make CertificateController implements Controller interface

--- src/presentation/controllers/certificate.ts
+++ src/presentation/controllers/certificate.ts
@@ -1,8 +1,9 @@
 import { HttpRequest, HttpResponse } from '../protocols/http';
 import { MissingParamError } from '../errors/missing-param-error';
 import { badRequest } from '../helpers/http-helper';
+import { Controller } from '../protocols/controller'
 
-export class CertificateController {
+export class CertificateController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['studentId', 'studentEmail', 'activePlan'];
     for (const field of requiredFields) {
