881b556c599b2bccbcd3786c23bbbf65d778b681
zoccalucas <lucaszoccasoares@gmail.com>
2023-05-22 19:55:09 -0300

    feat: ensure CertificateController returns 500 if EmailValidator throws

--- src/presentation/controllers/certificate.ts
+++ src/presentation/controllers/certificate.ts
@@ -2,8 +2,9 @@ import { HttpRequest, HttpResponse } from '../protocols/http';
 import { MissingParamError } from '../errors/missing-param-error';
 import { InvalidParamError } from '../errors/invalid-param-error';
 import { badRequest } from '../helpers/http-helper';
-import { Controller } from '../protocols/controller'
+import { Controller } from '../protocols/controller';
 import { EmailValidator } from '../protocols/email-validator';
+import { ServerError } from '../errors/server-error';
 
 export class CertificateController implements Controller {
   private readonly emailValidator: EmailValidator;
@@ -13,21 +14,30 @@ export class CertificateController implements Controller {
   }
 
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['studentId', 'studentEmail', 'activePlan'];
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field));
+    try {
+      const requiredFields = ['studentId', 'studentEmail', 'activePlan'];
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
-    }
 
-    const isValidStudentEmail = this.emailValidator.isValid(httpRequest.body.studentEmail);
-    if(!isValidStudentEmail) {
-      return badRequest(new InvalidParamError('studentEmail'));
-    }
+      const isValidStudentEmail = this.emailValidator.isValid(
+        httpRequest.body.studentEmail,
+      );
+      if (!isValidStudentEmail) {
+        return badRequest(new InvalidParamError('studentEmail'));
+      }
 
-    return {
-      statusCode: 200,
-      body: 'Success',
-    };
+      return {
+        statusCode: 200,
+        body: 'Success',
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError(),
+      };
+    }
   }
 }
